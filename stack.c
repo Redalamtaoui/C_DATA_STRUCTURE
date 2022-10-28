4a53f5f9934b84c3a20fd571972b9d5d897d5052
Reda LAMTAOUI <84287455+Redalamtaoui@users.noreply.github.com>
2022-10-28 02:37:32 +0200

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define MAX 50
+
+//function nedd : push , pop , getTop  , isEmpty
+int stack[MAX];
+int top = 0;
+void Push(int data){
+
+	if(top == MAX)
+		printf("STACK_OVER_FLOWE");
+	else{
+	top++;
+	stack[top] = data;
+	}
+
+
+}
+
+int isEmpty(){
+if(stack[top - 1] == '\0')
+return 1;
+
+return 0;
+}
+
+
+void Pop(){
+if(isEmpty() == 1)
+	printf("STACK_IS_EMPTY");
+else{
+stack[top] = '\0';
+top--;
+}
+	
+}
+
+int getTop()
+	
+{
+return stack[top];	
+}
+
+
+int main(){
+Push(4);
+Push(2);
+printf("%d - %d" , stack[1] , stack[2]);
+
+printf("\n%d\n" , getTop());
+
+
+}
